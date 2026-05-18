c4827117834e3ab2f0131d585ea8ea9d5a0e016b
walteringlezadv <walter.inglezadv@gmail.com>
2026-05-18 17:13:04 -0300

    fix: adiciona pre-rendering react-snap para indexação Google

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
+import { hydrateRoot, createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root")!;
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, <App />);
+} else {
+  createRoot(rootElement).render(<App />);
+}
