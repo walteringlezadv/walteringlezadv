730e8abe7e76f04c2efdc10dc0b05fc206db7da2
walteringlezadv <walter.inglezadv@gmail.com>
2026-05-24 13:41:59 -0300

    fix: remove sitemap temporariamente Branch astro-migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://blog.walteringlezadv.com.br',
   integrations: [
     react(),
     tailwind(),
-    sitemap(),
   ],
   output: 'static',
 });
